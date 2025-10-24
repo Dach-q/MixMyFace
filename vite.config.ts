1df1d34a0a8131f89ed05f67659f87503c0581ca
Dach-q <civilst8@gmail.com>
2025-10-24 15:12:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MixMyface/', // <= tačno ime repoa iz URL-a (case-sensitive)
+  base: '/MixMyface/'
 })
