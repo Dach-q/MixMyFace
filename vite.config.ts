8daa7d8e7bcded9d3e2c869305d9902502c6ca57
Dach-q <civilst8@gmail.com>
2025-10-24 14:32:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// STAVI TAČNO NAZIV TVOG REPOA OVDE umesto 'mixmyface-mvp'
 export default defineConfig({
   plugins: [react()],
-  base: '/mixmyface-mvp/',
+  base: '/MixMyFace/',   // tačno ime repoa
 })
